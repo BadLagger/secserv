5bd3ff919a87e7f9c00c6491721ba52dd641b3df
Vladimir Hrechko <grechko_vo@navis.ru>
2025-10-07 10:56:57 +0300

    feat: добавил логгер

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import "secserv/utils"
 
 func main() {
-	fmt.Printf("Secserv Run!\n")
+	log := utils.GlobalLogger().SetLevel(utils.Debug)
+	defer log.Log(utils.Info, "App DONE!!!")
+
+	log.Log(utils.Info, "App run!!!")
 }
